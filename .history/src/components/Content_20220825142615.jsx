b649a00919e7adc62baf0f178f033dae51c7512a
ellisvelandia <eyis619@gmail.com>
2022-08-25 14:27:15 -0500

    football

---
+++ .history/src/components/Content_20220825142615.jsx
@@ -0,0 +1,40 @@
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import "../App";
+
+const Content = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "https://www.scorebat.com/video-api/v3/feed/?token=[MjQ2MDJfMTY2MTQ1MDk2NF84OTM4NDA2YjIyNDgzMDgwYTVjZTY4NTdlMmI5MmJiMjgxMzc4MDJj]",
+    })
+      .then((res) => {
+        console.log(res.data);
+        setData(res.data.response);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  return (
+    <div className="content-container">
+      {data.map((item) => (
+        <div
+          className="video-div"
+          key={item.title}
+          onClick={() => window.open(item.competitionUrl)}
+        >
+          <div>
+            <h4>{item.title}</h4>
+          </div>
+          <div>
+            <img src={item.thumbnail} alt="#" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Content;
