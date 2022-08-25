472b88c75a572e864852ba57dd9f386113ec948e
ellisvelandia <eyis619@gmail.com>
2022-08-25 13:48:48 -0500

    football

--- .history/src/App_20220825134718.js
+++ .history/src/App_20220825134718.js
@@ -1,14 +1,11 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import Content from './components/Content';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Content/>
-     <Footer/>
+     
     </div>
   );
 }
--- .history/src/App_20220825134733.js
+++ .history/src/App_20220825134733.js
@@ -6,9 +6,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Content/>
-     <Footer/>
+     Na<></>
     </div>
   );
 }
